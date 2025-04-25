4002391c1d5b9b36a6fbac70a5fe2850b012387c
taiwan-jjl <taiwan.jjl@gmail.com>
2025-04-26 01:29:07 +0800

    init values

--- C/main.c
+++ C/main.c
@@ -1,5 +1,6 @@
 #include "basic_lanczos.h"  // Include the header for the basic_lancaos function
 #include <stdio.h>
+#include <string.h>
 #include "mkl.h"
 #include "helper.h"         // Include the helper functions
 
@@ -22,9 +23,20 @@ int main(void) {
     //########## END Intel MKL Conditional Numerical Reproducibility Control #####
     
 
+    //########## init values ##########
+    int A_dim = 3;
+    int A_ent = A_dim*A_dim;
+    double *A = (double*) mkl_calloc(A_ent, sizeof(double), 64);
+    double vals[9] = {4.0, 1.0, 0.0, 1.0, 3.0, 1.0, 0.0, 1.0, 2.0};
+    memcpy(A, vals, sizeof(vals));
+
+    for (int i = 0; i < A_ent; ++i) {
+        printf("A[%d] = %.1f\n", i, A[i]);
+    }
 
 
 
+    // void cblas_dgemv (const CBLAS_LAYOUT Layout, const CBLAS_TRANSPOSE trans, const MKL_INT m, const MKL_INT n, const double alpha, const double *a, const MKL_INT lda, const double *x, const MKL_INT incx, const double beta, double *y, const MKL_INT incy);
 
 
 
@@ -36,6 +48,13 @@ int main(void) {
 
     basic_lanczos();
 
+
+
+
+    //########## free memory ##########
+    mkl_free(A);
+
+    mkl_finalize();
 
     return 0;
 }
