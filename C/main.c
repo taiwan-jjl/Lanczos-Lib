aa0e2e5103d2d450546543ef1d19ecd2c7f17374
taiwan-jjl <taiwan.jjl@gmail.com>
2025-05-15 02:08:17 +0800

    verification

--- C/main.c
+++ C/main.c
@@ -24,7 +24,7 @@ int main(void) {
     //########## END Intel MKL Conditional Numerical Reproducibility Control #####
     
 
-    //########## init values ##########
+    //########## START init values ##########
 
     // Lanczos algorithm requires: A(input matrix), omega(pre-Lanczos vector), alpha(tridia-matrix), beta(tridia-matrix), nu(Lanczos vector),  
 
@@ -65,16 +65,27 @@ int main(void) {
     printf("Lanczos iteration stop criterion: %.20e\n", Lanczos_stop_crit); // print out the Lanczos iteration stop criterion.
 
     // Set Lanczos stop criterion check frequency. It is a balance between performance and criterion check.
-    const int Lanczos_stop_check_freq = 100;
+    const int Lanczos_stop_check_freq = 1;
 
     // helper variable "int Lanczos_iter": how many iterations executed
     int Lanczos_iter = 0;
+
+    //########## END init values ##########
 
 
     // Run Lanczos algorithm.
     basic_lanczos(A, nu, omega, alpha, beta, A_dim, Lanczos_stop_crit, Lanczos_stop_check_freq, &Lanczos_iter);
 
-    printf("Lanczos completed in %d iterations.\n", Lanczos_iter);
+
+    //########## START verification ##########
+
+    printf("Lanczos completed in %d iterations.\n", Lanczos_iter);          // Check Lanczos iteration completed number.
+    print_array_float("omega", omega, 0, A_ent);                            // verify "omega".
+    print_array_float("alpha", alpha, 0, A_dim);                            // verify "alpha".
+    print_array_float("beta", beta, 0, A_dim+1);                            // verify "beta".
+    print_array_float("nu", nu, 0, A_dim*(A_dim+2));                        // verify "nu".
+
+    //########## END verification ##########
 
 
     //########## free memory ##########
